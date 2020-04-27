bd82768bd551c918b79fd9ca4bae54bcdc89f2b1
YuriiTrofimenko <tyaa@ukr.net>
2020-04-27 19:27:23 +0300

    init plus demo

---
+++ HelloMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿namespace HelloMVVM.ViewModels
+{
+    using Catel.Data;
+    using Catel.MVVM;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public static readonly PropertyData FirstNameProperty;
+        public Command ShowFirstName { get; private set; }
+        static MainWindowViewModel()
+        {
+            FirstNameProperty
+                = RegisterProperty(nameof(FirstName), typeof(string), null);
+        }
+
+        public MainWindowViewModel()
+        {
+            ShowFirstName = new Command(OnShowFirstNameExecute, OnShowFirstNameCanExecute);
+        }
+
+        public override string Title { get { return "Welcome to HelloMVVM"; } }
+
+        public string FirstName
+        {
+            get { return GetValue<string>(FirstNameProperty); }
+            set { SetValue(FirstNameProperty, value); }
+        }
+
+        // TODO: Register models with the vmpropmodel codesnippet
+        // TODO: Register view model properties with the vmprop or vmpropviewmodeltomodel codesnippets
+        // TODO: Register commands with the vmcommand or vmcommandwithcanexecute codesnippets
+
+        private void OnShowFirstNameExecute()
+        {
+            MessageBox.Show(FirstName);
+        }
+
+        private bool OnShowFirstNameCanExecute()
+        {
+            return true;
+        }
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            // TODO: subscribe to events here
+        }
+
+        protected override async Task CloseAsync()
+        {
+            // TODO: unsubscribe from events here
+
+            await base.CloseAsync();
+        }
+    }
+}
---
+++ HelloMVVM/Views/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloMVVM.Views
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
